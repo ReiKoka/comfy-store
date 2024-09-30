7728c0c638bae3c0d337800f381b62bfcd1d83ac
ReiKoka <reikoka1@outlook.com>
2024-09-30 23:04:54 +0200

    Deployment fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,16 @@ import { Provider } from "react-redux";
 import { store } from "./store.ts";
 import { Toaster } from "sonner";
 
-
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
     <Toaster
-        position="top-center"
-        duration={3000}
-        toastOptions={{
-          className:
-            "text-foreground dark:bg-primary dark:text-background dark:shadow-[0px_0px_10px_2px_#1E293B]",
-        }}
-      />
-  </Provider>
+      position="top-center"
+      duration={3000}
+      toastOptions={{
+        className:
+          "text-foreground dark:bg-primary dark:text-background border-none",
+      }}
+    />
+  </Provider>,
 );
